1b8a5ffced4a5d4f6c869d7bfd55ce81b6eff666
viiveli <veli.viisanen@pm.me>
2022-12-01 19:59:25 +0200

    day one!

---
+++ build/day01.js
@@ -0,0 +1,15 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const fs_1 = require("fs");
+let input = (0, fs_1.readFileSync)('./inputs/day01/input', 'utf8');
+let input_arr = input.split('\n\n');
+let cal_array = [];
+input_arr.forEach(element => {
+    let elf_inventory = element.split('\n').map(i => Number(i));
+    let elf_calories = elf_inventory.reduce((a, c) => { return a + c; });
+    cal_array.push(elf_calories);
+});
+cal_array = cal_array.sort((a, b) => b - a);
+let sum_of_top_three = cal_array.slice(undefined, 3).reduce((a, c) => a + c);
+console.log(`Calories carried by the Elf with most calories: ${cal_array[0]}`);
+console.log(`Total calories carried by the three Elves with the most calories: ${sum_of_top_three}`);
