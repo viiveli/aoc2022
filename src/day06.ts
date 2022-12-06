52a7da99d6069e7b955ea9cbaf453a6e71691649
viiveli <veli.viisanen@pm.me>
2022-12-06 08:35:09 +0200

    refactor both parts to single solver

--- src/day06.ts
+++ src/day06.ts
@@ -2,29 +2,17 @@ import { readFileSync } from 'fs';
 
 let INPUT = readFileSync('./inputs/day06.in', 'utf8')
 
-const partOne = (dataStream: string) => {
-    // find the first occurrence of four characters that are all different
+const solver = (dataStream: string, length: number) => {
     for (let index = 0; index < dataStream.length; index++) {
-        const startOfPacket = new Set(dataStream.slice(index, index+4));
+        const marker = new Set(dataStream.slice(index, index+length));
         
-        if (startOfPacket.size === 4) {
-            console.log(index+4);
-            break;
+        if (marker.size === length) {
+            return index+length
         }
     }
+
+    return null;
 }
 
-const partTwo = (dataStream: string) => {
-    // find the first occurrence of 14 characters that are all different
-    for (let index = 0; index < dataStream.length; index++) {
-        const startOfMessage = new Set(dataStream.slice(index, index+14));
-        
-        if (startOfMessage.size === 14) {
-            console.log(index+14);
-            break;
-        }
-    }
-}
-
-partOne(INPUT)
-partTwo(INPUT)
+console.log(solver(INPUT, 4))
+console.log(solver(INPUT, 14))
